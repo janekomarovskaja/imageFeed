2273791da038e779363dae05dddd5baff1b62536
janekomarovskaja <jane.komarovskaja@gmail.com>
2025-05-05 18:29:18 +0300

    Initial Commit

---
+++ imageFeedTests/imageFeedTests.swift
@@ -0,0 +1,17 @@
+//
+//  imageFeedTests.swift
+//  imageFeedTests
+//
+//  Created by Женя Комаровская on 05.05.2025.
+//
+
+import Testing
+@testable import imageFeed
+
+struct imageFeedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
