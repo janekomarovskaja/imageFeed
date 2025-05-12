95f9aab510f4035fbb50d55924f61345a26171c8
janekomarovskaja <jane.komarovskaja@gmail.com>
2025-05-12 23:32:15 +0300

    fix code

--- imageFeed/ImagesList/ImagesListViewController.swift
+++ imageFeed/ImagesList/ImagesListViewController.swift
@@ -65,14 +65,17 @@ extension ImagesListViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: ImagesListCell.reuseIdentifier,
+            for: indexPath
+        )
             
-            guard let imageListCell = cell as? ImagesListCell else {
-                return UITableViewCell()
-            }
+        guard let imageListCell = cell as? ImagesListCell else {
+            return UITableViewCell()
+        }
             
         configCell(for: imageListCell, with: indexPath)
-            return imageListCell
-        }
+        return imageListCell
+    }
     
 }
