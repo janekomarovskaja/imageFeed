6491cc3be92e83fe43186d675403f81ce4549d55
janekomarovskaja <jane.komarovskaja@gmail.com>
2025-05-19 22:26:54 +0300

    add profileView

---
+++ imageFeed/ProfileView/ProfileViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet var exitButton: UIButton!
+    @IBOutlet var userPicture: UIImageView!
+    @IBOutlet var username: UILabel!
+    @IBOutlet var userNickname: UILabel!
+    @IBOutlet var userDescription: UILabel!
+}
