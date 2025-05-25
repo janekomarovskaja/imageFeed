915837d5e92a1ae3c22cc4cccbe41cad21c9d5f3
janekomarovskaja <jane.komarovskaja@gmail.com>
2025-05-25 13:04:28 +0300

    fix profile view and image list controller

--- imageFeed/ImagesList/ImagesListViewController.swift
+++ imageFeed/ImagesList/ImagesListViewController.swift
@@ -7,11 +7,29 @@ final class ImagesListViewController: UIViewController {
     private let currentDate = Date()
     private let likeButtonActiveImage = "LikeButtonActive"
     private let likeButtonNoActiveImage = "LikeButtonNoActive"
+    private let showSingleImageSegueIdentifier = "ShowSingleImage"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.separatorStyle = .none
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == showSingleImageSegueIdentifier {
+            guard
+                let viewController = segue.destination as? SingleImageViewController,
+                let indexPath = sender as? IndexPath
+            else {
+                assertionFailure("Invalid segue destination")
+                return
+            }
+            
+            let image = UIImage(named: photosName[indexPath.row])
+            viewController.image = image
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
     }
 }
 
@@ -37,7 +55,9 @@ extension ImagesListViewController {
 }
 
 extension ImagesListViewController: UITableViewDelegate {
-    // TO DO: func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: showSingleImageSegueIdentifier, sender: indexPath)
+    }
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         guard let image = UIImage(named: photosName[indexPath.row]) else {
--- imageFeed/ProfileView/ProfileViewController.swift
+++ imageFeed/ProfileView/ProfileViewController.swift
@@ -1,10 +1,73 @@
 import UIKit
 
 final class ProfileViewController: UIViewController {
+    let exitButtonPictureName = "exitButton"
+    let userPictureName = "userPic"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let userPicture = UIImage(named: userPictureName)
+        let imageView = UIImageView(image: userPicture)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.cornerRadius = 61
+        view.addSubview(imageView)
+        imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 70).isActive = true
+                
+        let userName = UILabel()
+        userName.text = "Екатерина Новикова"
+        userName.font = UIFont.systemFont(ofSize: 23, weight: .bold)
+        userName.textColor = .ypWhite
+        userName.numberOfLines = 0
+        userName.lineBreakMode = .byWordWrapping
+        userName.sizeToFit()
+        userName.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(userName)
+        userName.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+        userName.trailingAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true
+        userName.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8).isActive = true
+        
+        let userNickname = UILabel()
+        userNickname.text = "@ekaterina_nov"
+        userNickname.textColor = .ypGray
+        userNickname.font = UIFont.systemFont(ofSize: 13)
+        userNickname.numberOfLines = 0
+        userNickname.lineBreakMode = .byWordWrapping
+        userNickname.sizeToFit()
+        userNickname.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(userNickname)
+        userNickname.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+        userNickname.trailingAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true
+        userNickname.topAnchor.constraint(equalTo: userName.bottomAnchor, constant: 8).isActive = true
+        
+        let userDescription = UILabel()
+        userDescription.text = "Hello, world!"
+        userDescription.textColor = .ypWhite
+        userDescription.font = UIFont.systemFont(ofSize: 13)
+        userDescription.numberOfLines = 0
+        userDescription.lineBreakMode = .byWordWrapping
+        userDescription.sizeToFit()
+        userDescription.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(userDescription)
+        userDescription.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+        userDescription.trailingAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true
+        userDescription.topAnchor.constraint(equalTo: userNickname.bottomAnchor, constant: 8).isActive = true
+        
+        let exitButton = UIButton.systemButton(
+            with: UIImage(named: exitButtonPictureName)!,
+                    target: self,
+                    action: #selector(Self.didTapButton)
+                )
+        exitButton.tintColor = .ypRed
+        exitButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(exitButton)
+        exitButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
+        exitButton.centerYAnchor.constraint(equalTo: imageView.centerYAnchor).isActive = true
+            
+    }
     
-    @IBOutlet var exitButton: UIButton!
-    @IBOutlet var userPicture: UIImageView!
-    @IBOutlet var username: UILabel!
-    @IBOutlet var userNickname: UILabel!
-    @IBOutlet var userDescription: UILabel!
+    @objc private func didTapButton(_ sender: UIButton) {}
 }
