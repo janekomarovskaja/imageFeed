05d00a0e17dd8c306dd19a998a5c1804fdf6ebcd
janekomarovskaja <jane.komarovskaja@gmail.com>
2025-06-03 23:11:02 +0300

    fix code

--- imageFeed/Constants.swift
+++ imageFeed/Constants.swift
@@ -1,18 +1,9 @@
 import Foundation
 
-enum UrlError: Error {
-    case invalidBaseURL
-}
-
 enum Constants {
     static let accessKey = "-exAa4gZtSkpwsFhbW_ROuyqg_NL-LxG2-VKGoVRiYc"
     static let secretKey = "rmUfWevEhmgM77FU8DoZD0RhlJpElqn4f1Ff5PblC6k"
     static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
     static let accessScope = "public+read_user+write_likes"
-    static func makeBaseURL() throws -> URL {
-        guard let defaultBaseURL = URL(string: "https://api.unsplash.com") else {
-            throw UrlError.invalidBaseURL
-        }
-        return defaultBaseURL
-    }
+    static let defaultBaseURL: URL? = URL(string: "https://api.unsplash.com")
 }
